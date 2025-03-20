db3f64263389365d4970cc8525c6da4abc904fbc
mariimoreiraa <jandricicmariana900@gmail.com>
2025-03-20 11:20:51 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    basic.showNumber(1)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showNumber(3)
+    basic.pause(1000)
+    basic.showNumber(4)
+    basic.pause(1000)
+    basic.showNumber(5)
+})
